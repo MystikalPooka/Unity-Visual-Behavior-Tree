68df049c255a0fe32b4fe4c814062b1cf63cd796
MystikalPooka <mystikalpooka@gmail.com>
2018-04-13 02:46:33 -0600

    Working Drawn Debug Boxes

--- Assets/Editor/TreeDebuggerWindow.cs
+++ Assets/Editor/TreeDebuggerWindow.cs
@@ -45,14 +45,18 @@ namespace Assets.Editor
         private void OnGUI()
         {
             if (!Initialized) Initialize();
-            TopToolbar(TopToolbarRect);
 
-            TreeLogArea(EditorGUILayout.GetControlRect());
+            using (new EditorGUILayout.VerticalScope())
+            {
+                TopToolbar(TopToolbarRect);
+                TreeLogArea(TreeDrawArea);
+            }
         }
 
         private bool Initialized = false;
         private void Initialize()
         {
+            this.autoRepaintOnSceneChange = true;
             Initialized = true;
         }
 
@@ -82,6 +86,7 @@ namespace Assets.Editor
         {
             ManagerName.SetValueAndForceNotify((string)name);
             LogDrawers.Clear();
+            rowTotalDrawn.Clear();
             LogDrawers = new Dictionary<int, BehaviorLogDrawer>();
         }
 
@@ -92,22 +97,29 @@ namespace Assets.Editor
                 .Where(x => x.LoggerName == ManagerName.Value)
                 .Do(x =>
                 {
-                    var depth = x.State.Depth;
-                    if(!rowTotalDrawn.ContainsKey(depth))
-                    {
-                        rowTotalDrawn.Add(depth,0);
-                    }
-                    else
-                    {
-                        ++rowTotalDrawn[depth];
-                    }
+
                     //Keep the rects the same as when they were first created
                     if (!LogDrawers.ContainsKey(x.BehaviorID))
                     {
-                        float rectX = ((BehaviorLogRectSize.x/2) * rowTotalDrawn[depth]);
-                        float rectY = BehaviorLogRectSize.y * (depth < 0 ? 0 : depth*1.1f);
-                        var position = new Vector2(rectX, rectY);
-                        var drawRect = new Rect(position, BehaviorLogRectSize);
+                        var depth = x.State.Depth;
+
+                        if (!rowTotalDrawn.ContainsKey(depth))
+                        {
+                            rowTotalDrawn.Add(depth, 0);
+                        }
+                        else
+                        {
+                            rowTotalDrawn[depth] = rowTotalDrawn[depth] + 1;
+                        }
+
+                        Debug.Log("rowTotal[" + depth + "] = " + rowTotalDrawn[depth]);
+                        Debug.Log("Depth: " + depth);
+
+                        float rectX = ((BehaviorLogRectSize.x) * rowTotalDrawn[depth]) + 10;
+                        float rectY = BehaviorLogRectSize.y * (depth < 0 ? 0 : depth+1)+20;
+                        var pos = new Vector2(rectX, rectY);
+                        Debug.LogWarning("position: " + pos);
+                        var drawRect = new Rect(pos, BehaviorLogRectSize);
 
                         LogDrawers.Add(x.BehaviorID, new BehaviorLogDrawer(x.LoggerName, x.BehaviorID, drawRect));
                     }
@@ -118,6 +130,7 @@ namespace Assets.Editor
             {
                 log.Value.DrawBehaviorLogEntry();
             }
+
         }
 
         private void OnDestroy()
